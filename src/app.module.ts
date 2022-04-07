d84dc1574ce2647b6098f2f8f7bfe1fc5b6cf97f
ajiways <ajiwayswc@gmail.com>
2022-04-08 04:26:23 +0700

    moved schema file due to system error (wtf)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { BookModule } from './book/book.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from './config/typeorm';
 import { AuthorModule } from './author/author.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -13,11 +14,12 @@ import { AuthorModule } from './author/author.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'src/schemas/schema.gql'),
       sortSchema: true,
     }),
     BookModule,
     AuthorModule,
+    CommentModule,
   ],
   controllers: [],
   providers: [],
